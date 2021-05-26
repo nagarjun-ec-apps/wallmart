61a86314029e22f19b676419eb084978b8fd7307
nagarjunreddy326 <nagarjunreddy326@gmail.com>
2021-05-26 23:47:01 +0000

    updated

--- nag.sh
+++ nag.sh
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
 
-<<<<<<< HEAD
 
 git commands are very imp..
-=======
-=======
+
+my frind
 my frind 
->>>>>>> fcf6000... updated
+
 Mmahaboob
 hello guys git commanda are very imp...
 we are seeing
 we are updating
-<<<<<<< HEAD
-
-=======
->>>>>>> fcf6000... updated
 i am kidding
 we are adding 
 reddy
 maharjun 
 updated
 hi guys
-<<<<<<< HEAD
-=======
->>>>>>> 60dabc5... updated
->>>>>>> fcf6000... updated
+
