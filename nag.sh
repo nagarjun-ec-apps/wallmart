9eaf9e6d00f7bf458f541a07522d49dfdd32dd06
nagarjunreddy326 <nagarjunreddy326@gmail.com>
2021-05-26 06:40:53 +0000

    updated

--- nag.sh
+++ nag.sh
@@ -1,4 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+<<<<<<< HEAD
+>>>>>>> bcfeb14... updated
 git commands are very imp..
 =======
 Mmahaboob
@@ -6,6 +11,10 @@ hello guys git commanda are very imp...
 we are seeing
 we are updating
 >>>>>>> cd50d9e... updated
+<<<<<<< HEAD
+=======
+>>>>>>> 15d6ffd... updaded
+>>>>>>> bcfeb14... updated
 i am kidding
 we are adding 
 reddy
